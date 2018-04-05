0ac854d846a8a32491523db33ce5f68333ca2cf6
Jin Yamanaka <jiny2001@users.noreply.github.com>
2018-04-04 22:01:17 -0700

    update pre-built batch data loading function

--- helper/loader.py
+++ helper/loader.py
@@ -256,7 +256,7 @@ class DataSets:
 				return False
 			if config.getboolean("batch", "jpeg_mode") != self.jpeg_mode:
 				return False
-			if config.getint("batch", "max_value") != self.max_value:
+			if config.getfloat("batch", "max_value") != self.max_value:
 				return False
 
 			return True
