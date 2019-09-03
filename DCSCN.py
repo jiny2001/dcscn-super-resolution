c7f9054bc06192d9c1f4770c5e25418ea1c91f88
Ethan <Ereish02@gmail.com>
2019-09-03 10:35:26 -0500

    add multi-threaded batch image loading plus fixes
    Fixed multi-threaded batch image generator and added multi-threaded batch image loader for large datasets

--- DCSCN.py
+++ DCSCN.py
@@ -17,16 +17,30 @@ import math
 import os
 import time
 
+from helper import loader, tf_graph, utilty as util
+
 import numpy as np
 import tensorflow as tf
 
-from helper import loader, tf_graph, utilty as util
 
 BICUBIC_METHOD_STRING = "bicubic"
 
 INPUT_IMAGE_DIR = "input"
 INTERPOLATED_IMAGE_DIR = "interpolated"
 TRUE_IMAGE_DIR = "true"
+
+def make_parallel(fn, num_gpus, **kwargs):
+    in_splits = {}
+    for k, v in kwargs.items():
+        in_splits[k] = tf.split(v, num_gpus)
+
+    out_split = []
+    for i in range(num_gpus):
+        with tf.device(tf.DeviceSpec(device_type="GPU", device_index=i)):
+            with tf.variable_scope(tf.get_variable_scope(), reuse=tf.AUTO_REUSE):
+                out_split.append(fn(**{k : v[i] for k, v in in_splits.items()}))
+
+    return tf.concat(out_split, axis=0)
 
 class SuperResolution(tf_graph.TensorflowGraph):
     def __init__(self, flags, model_name=""):
--- train.py
+++ train.py
@@ -8,118 +8,121 @@ DCSCN training functions.
 Testing Environment: Python 3.6.1, tensorflow >= 1.3.0
 """
 
-import logging
-import sys
-
-import tensorflow as tf
-
-import DCSCN
-from helper import args, utilty as util
-
-FLAGS = args.get()
-
-
-def main(not_parsed_args):
-    if len(not_parsed_args) > 1:
-        print("Unknown args:%s" % not_parsed_args)
-        exit()
-
-    model = DCSCN.SuperResolution(FLAGS, model_name=FLAGS.model_name)
-
-    if FLAGS.build_batch:
-        model.load_datasets(FLAGS.data_dir + "/" + FLAGS.dataset, FLAGS.batch_dir + "/" + FLAGS.dataset,
-                            FLAGS.batch_image_size, FLAGS.stride_size)
-    else:
-        model.load_dynamic_datasets(FLAGS.data_dir + "/" + FLAGS.dataset, FLAGS.batch_image_size)
-    model.build_graph()
-    model.build_optimizer()
-    model.build_summary_saver()
-
-    logging.info("\n" + str(sys.argv))
-    logging.info("Test Data:" + FLAGS.test_dataset + " Training Data:" + FLAGS.dataset)
-    util.print_num_of_total_parameters(output_to_logging=True)
-
-    total_psnr = total_ssim = 0
-
-    for i in range(FLAGS.tests):
-        psnr, ssim = train(model, FLAGS, i)
-        total_psnr += psnr
-        total_ssim += ssim
-
-        logging.info("\nTrial(%d) %s" % (i, util.get_now_date()))
-        model.print_steps_completed(output_to_logging=True)
-        logging.info("PSNR:%f, SSIM:%f\n" % (psnr, ssim))
-
-    if FLAGS.tests > 1:
-        logging.info("\n=== Final Average [%s] PSNR:%f, SSIM:%f ===" % (
-            FLAGS.test_dataset, total_psnr / FLAGS.tests, total_ssim / FLAGS.tests))
-
-    model.copy_log_to_archive("archive")
-
-
-def train(model, flags, trial):
-    test_filenames = util.get_files_in_directory(flags.data_dir + "/" + flags.test_dataset)
-    if len(test_filenames) <= 0:
-        print("Can't load images from [%s]" % (flags.data_dir + "/" + flags.test_dataset))
-        exit()
-
-    model.init_all_variables()
-    if flags.load_model_name != "":
-        model.load_model(flags.load_model_name, output_log=True)
-
-    model.init_train_step()
-    model.init_epoch_index()
-    model_updated = True
-
-    psnr, ssim = model.evaluate(test_filenames)
-    model.print_status(psnr, ssim, log=True)
-    model.log_to_tensorboard(test_filenames[0], psnr, save_meta_data=True)
-
-    while model.lr > flags.end_lr:
-
-        model.build_input_batch()
-        model.train_batch()
-
-        if model.training_step * model.batch_num >= model.training_images:
-
-            # one training epoch finished
-            model.epochs_completed += 1
-            psnr, ssim = model.evaluate(test_filenames)
-            model.print_status(psnr, ssim, log=model_updated)
-            model.log_to_tensorboard(test_filenames[0], psnr, save_meta_data=model_updated)
-            model.save_model(trial=trial, output_log=False)
-
-            model_updated = model.update_epoch_and_lr()
-            model.init_epoch_index()
-
-    model.end_train_step()
-
-    # save last generation anyway
-    model.save_model(trial=trial, output_log=True)
-
-    # outputs result
-    evaluate_model(model, flags.test_dataset)
-
-    if FLAGS.do_benchmark:
-        for test_data in ['set5', 'set14', 'bsd100']:
-            if test_data != flags.test_dataset:
-                evaluate_model(model, test_data)
-
-    return psnr, ssim
-
-
-def evaluate_model(model, test_data):
-    test_filenames = util.get_files_in_directory(FLAGS.data_dir + "/" + test_data)
-    total_psnr = total_ssim = 0
-
-    for filename in test_filenames:
-        psnr, ssim = model.do_for_evaluate_with_output(filename, output_directory=FLAGS.output_dir, print_console=False)
-        total_psnr += psnr
-        total_ssim += ssim
-
-    logging.info("Model Average [%s] PSNR:%f, SSIM:%f" % (
-        test_data, total_psnr / len(test_filenames), total_ssim / len(test_filenames)))
-
 
 if __name__ == '__main__':
-    tf.app.run()
+    import logging
+    import sys
+
+    import tensorflow as tf
+    import DCSCN
+    from helper import args, utilty as util
+
+    mirrored_stratagy = tf.contrib.distribute.MirroredStrategy()
+    with mirrored_stratagy.scope():
+        FLAGS = args.get()
+
+        def main(not_parsed_args):
+            if len(not_parsed_args) > 1:
+                print("Unknown args:%s" % not_parsed_args)
+                exit()
+
+            model = DCSCN.SuperResolution(FLAGS, model_name=FLAGS.model_name)
+
+            if FLAGS.build_batch:
+                model.load_datasets(FLAGS.data_dir + "/" + FLAGS.dataset, FLAGS.batch_dir + "/" + FLAGS.dataset,
+                                    FLAGS.batch_image_size, FLAGS.stride_size)
+            else:
+                model.load_dynamic_datasets(FLAGS.data_dir + "/" + FLAGS.dataset, FLAGS.batch_image_size)
+            model.build_graph()
+            model.build_optimizer()
+            model.build_summary_saver()
+
+            logging.info("\n" + str(sys.argv))
+            logging.info("Test Data:" + FLAGS.test_dataset + " Training Data:" + FLAGS.dataset)
+            util.print_num_of_total_parameters(output_to_logging=True)
+
+            total_psnr = total_ssim = 0
+
+            for i in range(FLAGS.tests):
+                psnr, ssim = train(model, FLAGS, i)
+                total_psnr += psnr
+                total_ssim += ssim
+
+                logging.info("\nTrial(%d) %s" % (i, util.get_now_date()))
+                model.print_steps_completed(output_to_logging=True)
+                logging.info("PSNR:%f, SSIM:%f\n" % (psnr, ssim))
+
+            if FLAGS.tests > 1:
+                logging.info("\n=== Final Average [%s] PSNR:%f, SSIM:%f ===" % (
+                    FLAGS.test_dataset, total_psnr / FLAGS.tests, total_ssim / FLAGS.tests))
+
+            model.copy_log_to_archive("archive")
+
+
+        def train(model, flags, trial):
+            test_filenames = util.get_files_in_directory(flags.data_dir + "/" + flags.test_dataset)
+            if len(test_filenames) <= 0:
+                print("Can't load images from [%s]" % (flags.data_dir + "/" + flags.test_dataset))
+                exit()
+
+            model.init_all_variables()
+            if flags.load_model_name != "":
+                model.load_model(flags.load_model_name, output_log=True)
+
+            model.init_train_step()
+            model.init_epoch_index()
+            model_updated = True
+
+            psnr, ssim = model.evaluate(test_filenames)
+            model.print_status(psnr, ssim, log=True)
+            model.log_to_tensorboard(test_filenames[0], psnr, save_meta_data=True)
+
+            while model.lr > flags.end_lr:
+
+                model.build_input_batch()
+                model.train_batch()
+
+                if model.training_step * model.batch_num >= model.training_images:
+
+                    # one training epoch finished
+                    model.epochs_completed += 1
+                    psnr, ssim = model.evaluate(test_filenames)
+                    model.print_status(psnr, ssim, log=model_updated)
+                    model.log_to_tensorboard(test_filenames[0], psnr, save_meta_data=model_updated)
+                    model.save_model(trial=trial, output_log=False)
+
+                    model_updated = model.update_epoch_and_lr()
+                    model.init_epoch_index()
+
+            model.end_train_step()
+
+            # save last generation anyway
+            model.save_model(trial=trial, output_log=True)
+
+            # outputs result
+            evaluate_model(model, flags.test_dataset)
+
+            if FLAGS.do_benchmark:
+                for test_data in ['set5', 'set14', 'bsd100']:
+                    if test_data != flags.test_dataset:
+                        evaluate_model(model, test_data)
+
+            return psnr, ssim
+
+
+        def evaluate_model(model, test_data):
+            test_filenames = util.get_files_in_directory(FLAGS.data_dir + "/" + test_data)
+            total_psnr = total_ssim = 0
+
+            for filename in test_filenames:
+                psnr, ssim = model.do_for_evaluate_with_output(filename, output_directory=FLAGS.output_dir, print_console=False)
+                total_psnr += psnr
+                total_ssim += ssim
+
+            logging.info("Model Average [%s] PSNR:%f, SSIM:%f" % (
+                test_data, total_psnr / len(test_filenames), total_ssim / len(test_filenames)))
+
+
+        if __name__ == '__main__':
+            tf.app.run()
+
